02a2f05a265a8f453710c64f2b9e5a9d08d78364
Tee <nico.J.64@hotmail.be>
2019-05-23 09:54:29 +0200

    Ajout méthodes library

--- Hackathon/Hackathon/Library.cs
+++ Hackathon/Hackathon/Library.cs
@@ -18,16 +18,30 @@ namespace Hackathon {
 
         }
 
-        public void AddItem(Item item) {
-            
+        public void AddItem(Item item) {//Add a given item
+            foreach (Item item_temp in Items) {
+                if (item.Equals(item_temp)) { }
+                else {
+                    Items.Add(item);
+                }
+            } 
         }
 
-        public void RemoveItem(Item item) {
-
+        public void RemoveItem(Item item) {//Remove a given item
+            foreach (Item item_temp in Items) {
+                if (item.Equals(item_temp)) {
+                    Items.Remove(item_temp);
+                } else { }
+            }
         }
 
-        public void ModifyItem(Item oldi, Item newi) {
-
+        public void ModifyItem(Item oldi, Item newi) {//replace an item with another
+            foreach(Item item_temp in Items) {
+                if (oldi.Equals(item_temp)) {
+                    RemoveItem(oldi);
+                    AddItem(newi);
+                } else { }
+            }
         }
     }
 }
