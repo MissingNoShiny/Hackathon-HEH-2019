81291506901ca940fa03e1de7b7decff27114d4c
Unknown <larcinvincent@gmail.com>
2019-05-23 03:13:48 +0200

    Structure de base partie logique

---
+++ Hackathon/Hackathon/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon {
+    public class Item {
+        public List<Attribute> Values {
+            get; private set;
+        }
+
+        public Item(List<Attribute> values) {
+            Values = values;
+        }
+
+        public List<DataType> getDataTypes() {
+            return null;
+        }
+    }
+}
---
+++ Hackathon/Hackathon/Library.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon {
+    public class Library {
+        public Dictionary<String, DataType> Attributes {
+            get; private set;
+        }
+        public List<Item> Items {
+            get; private set;
+        }
+        public String Name;
+
+        public Library() {
+
+        }
+
+        public void AddItem(Item item) {
+            
+        }
+
+        public void RemoveItem(Item item) {
+
+        }
+
+        public void ModifyItem(Item oldi, Item newi) {
+
+        }
+    }
+}
